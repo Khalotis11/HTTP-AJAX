c8144181e457d012e785166be48d88d2c90fa4c1
Khalotis11 <moekhaloti@gmail.com>
2019-06-26 02:41:46 +0100

    Day 1 MVP Completed

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React,{ Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <></>
-    </div>
-  );
-}
+import axios from "axios";
+import { Friends } from "./components/Friends"
 
-export default App;
+export default class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      friends: []
+    }
+  }
+  componentDidMount(){
+    axios
+    .get("http://localhost:5000/friends")
+    .then(res => {
+      console.log(res)
+      this.setState({
+        friends: res.data
+      })
+    })
+    .catch(err => {
+      console.log("Error:", err)
+    })
+    console.log(this.state)
+  }
+  render() {
+    return (
+      <div className="App">
+        <h3>Friends:</h3>
+        <Friends friends={this.state.friends} />
+      </div>
+    );
+  }
+}
